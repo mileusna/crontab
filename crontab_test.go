0b5a5e14ff6f1afe5a2fc227fc2d5fa916c844bb
mileusna <mileusna@gmail.com>
2017-08-10 02:14:56 +0200

    Add more syntax tests

--- crontab_test.go
+++ crontab_test.go
@@ -2,21 +2,21 @@ package crontab
 
 import "testing"
 
-var schTest = []struct {
-	s   string
-	cnt [5]int
-}{
-	{"* * * * *", [5]int{60, 24, 31, 12, 7}},
-	{"*/2 * * * *", [5]int{30, 24, 31, 12, 7}},
-	{"*/10 * * * *", [5]int{6, 24, 31, 12, 7}},
-	{"* * * * */2", [5]int{60, 24, 0, 12, 4}},
-	{"5,8,9 */2 2,3 * */2", [5]int{3, 12, 2, 12, 4}},
-	{"* 5-11 2-30/2 * *", [5]int{60, 7, 15, 12, 0}},
-	{"1,2,5-8 * * */3 *", [5]int{6, 24, 31, 4, 7}},
-}
-
 // TestSchedule parse the crontab syntax and compare number of target min/hour/days/month with expected ones
 func TestSchedule(t *testing.T) {
+	var schTest = []struct {
+		s   string
+		cnt [5]int
+	}{
+		{"* * * * *", [5]int{60, 24, 31, 12, 7}},
+		{"*/2 * * * *", [5]int{30, 24, 31, 12, 7}},
+		{"*/10 * * * *", [5]int{6, 24, 31, 12, 7}},
+		{"* * * * */2", [5]int{60, 24, 0, 12, 4}},
+		{"5,8,9 */2 2,3 * */2", [5]int{3, 12, 2, 12, 4}},
+		{"* 5-11 2-30/2 * *", [5]int{60, 7, 15, 12, 0}},
+		{"1,2,5-8 * * */3 *", [5]int{6, 24, 31, 4, 7}},
+	}
+
 	for _, sch := range schTest {
 		j, err := parseSchedule(sch.s)
 		if err != nil {
@@ -45,22 +45,24 @@ func TestSchedule(t *testing.T) {
 	}
 }
 
-var schErrorTest = []string{
-	"* * * * * *",
-	"0-70 * * * *",
-	"* 0-30 * * *",
-	"* * 1-40/2 * *",
-	"* * ab/2 * *",
-	"* * * 1-15 *",
-	"* * * * 7,8,9",
-	"1 2 3 4 5 6",
-	"* 1,2/10 * * *",
-	"* * 1,2,3,1-15/10 * *",
-	"a b c d e",
-}
-
 // TestScheduleError tests crontab syntax which should not be accepted
 func TestScheduleError(t *testing.T) {
+	var schErrorTest = []string{
+		"* * * * * *",
+		"0-70 * * * *",
+		"* 0-30 * * *",
+		"* * 0-10 * *",
+		"* * 0,1,2 * *",
+		"* * 1-40/2 * *",
+		"* * ab/2 * *",
+		"* * * 1-15 *",
+		"* * * * 7,8,9",
+		"1 2 3 4 5 6",
+		"* 1,2/10 * * *",
+		"* * 1,2,3,1-15/10 * *",
+		"a b c d e",
+	}
+
 	for _, s := range schErrorTest {
 		if _, err := parseSchedule(s); err == nil {
 			t.Error(s, "should be error", err)
